ebde7887139edd9622ad5fc3fbdba7a25cabb968
hyerin6 <hyerinn6@gmail.com>
2020-05-15 12:27:52 +0900

    [#38] swagger 적용

---
+++ src/main/java/com/hackday/timeline/config/swagger/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.hackday.timeline.config.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.google.common.base.Predicates;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+			.ignoredParameterTypes(AuthenticationPrincipal.class)
+			.select()
+			.apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+			.paths(Predicates.not(PathSelectors.regex("/error.*")))
+			.build();
+	}
+
+}
