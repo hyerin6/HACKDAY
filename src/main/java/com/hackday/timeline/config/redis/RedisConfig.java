8a6fa7946987cb0b6f107db54fb394c1955dd9b1
hyerin6 <hyerinn6@gmail.com>
2020-05-20 23:07:42 +0900

    [#89] 불필요한 코드 제거

--- src/main/java/com/hackday/timeline/config/redis/RedisConfig.java
+++ src/main/java/com/hackday/timeline/config/redis/RedisConfig.java
@@ -1,22 +1,16 @@
 package com.hackday.timeline.config.redis;
 
-import java.time.Duration;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Slf4j
